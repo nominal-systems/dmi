9c2178d90244b5a2ccb5c6de026fe2a95f58959a
monsieurbelbo <gonzalo.bellver@gmail.com>
2022-07-26 17:53:06 +0200

    Generating all sensitive data

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,8 @@
-variable "mysql_database" { }
-
-variable "mysql_username" { }
-
-variable "mysql_password" { }
-
 module "dev_cluster" {
   source                = "./main"
   env_name              = "dev"
   cluster_name          = "dmi-cluster"
-  mysql_database        = var.mysql_database
-  mysql_username        = var.mysql_username
-  mysql_password        = var.mysql_password
+  mysql_database        = "dmi"
+  mysql_username        = "dmi"
   mysql_public_access   = true
 }
